70327ef482140c19f694fa05850a0bac1072171b
Enzo <enzomortola@gmail.com>
2026-03-18 16:17:00 -0300

    PWA para google chrome

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+// Basic Service Worker for PWA installation
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Pass-through
+});
